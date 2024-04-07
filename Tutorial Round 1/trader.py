08a6d1391515422cca4605453f771242acf25c86
Humzah Merchant <hmerchan@usc.edu>
2024-04-07 14:20:57 -0700

    Created Template

---
+++ Tutorial Round 1/trader.py
@@ -0,0 +1,8 @@
+from datamodel import OrderDepth, UserId, TradingState, Order
+from typing import List
+import string
+
+class Trader:
+    def run(self, state: TradingState):
+        # Your code here
+        return [], 1, "hi"
