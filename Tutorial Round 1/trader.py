fd595d17485b843410256c468e57a98505028ade
HumzahUSC <hmerchan@usc.edu>
2024-04-07 17:05:37 -0700

    work
    work

--- Tutorial Round 1/trader.py
+++ Tutorial Round 1/trader.py
@@ -1,8 +1,20 @@
 from datamodel import OrderDepth, UserId, TradingState, Order
 from typing import List
 import string
+from copy import deepcopy
+
+empty_dict = {'AMETHYSTS': 0, 'STARFRUIT': 0}
+
 
 class Trader:
+    def ordersSimpleMarketMaking(self, product: str):
+        pass
+
+
     def run(self, state: TradingState):
-        # Your code here
-        return [], 1, "hi"
+    positions = deepcopy(empty_dict)
+
+        for product in state.position:
+            positions[product] = state.position[product]
+            
+
